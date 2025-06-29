bd4555245521b0c05e048840a9de7665bd516a13
MagnusSletten <magnus.elias.sletten@gmail.com>
2025-06-29 16:18:41 +0200

    Update dependencies

--- src/Frontend/src/App.js
+++ src/Frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import logo from './logo.svg';
 import BranchSelect from './BranchSelect';
 import Description from './Description';
 import { useImmer } from 'use-immer';
